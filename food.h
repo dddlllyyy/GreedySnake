78b68f856cc60ff120a69282fd7ead15947865cc
dddlllyyy <48828538+dddlllyyy@users.noreply.github.com>
2020-03-12 10:50:12 +0800

    Create food.h

---
+++ food.h
@@ -0,0 +1,27 @@
+#ifndef FOOD_H
+#define FOOD_H
+#include "snake.h"
+
+class Snake;
+class Food
+{
+public: 
+	Food() : cnt(0),flash_flag(false),big_flag(false),x(0),y(0),big_x(0),big_y(0),progress_bar(0){};
+	void DrawFood(Snake&);
+	void DrawBigFood(Snake&);
+	int GetCnt();
+	void FlashBigFood();
+	bool GetBigFlag();
+	int  GetProgressBar();
+
+private:
+	int cnt;
+	bool flash_flag; //闪烁标记
+	int  big_flag;  //有限时实物标记
+	int  x,y;
+	int big_x,big_y;
+	int progress_bar; //限时食物进度条
+	friend class Snake;
+
+};
+#endif
